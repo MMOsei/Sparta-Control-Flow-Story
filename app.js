d5b0229f4813edf57c459f2b46a86717cd198fe2
mmosei <mmanly-osei@spartaglobal.com>
2018-05-27 09:30:21 +0100

    created files and dev branch for the task

---
+++ app.js
@@ -0,0 +1,16 @@
+// Long weekend homework
+// Level 1 (Mandatory)
+//
+// To create a complex story using various control flow methods. It must:
+//
+// use every Data Type (integers, arrays, objects etc)
+// use if, if/else, if/else, if/else and Switch statements
+// use every comparison operator (<, >, <=, >= etc)
+// use at least one multiple condition per statement if( true && true ) OR if( true || false )
+// Use as many in-built functions as possible:
+// prompts and alerts are mandatory
+// other methods like length, reverse, indexOf, substr, and any others you can find
+// The story should be fun and entertaining!!
+// HINT: Plan well before you start coding.
+//
+// The Repo name is Sparta-Control-Flow-Story
