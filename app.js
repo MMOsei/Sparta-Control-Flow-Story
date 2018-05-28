1a06cbd71cfea6102216ab51e0f9ff98048afa67
mmosei <mmanly-osei@spartaglobal.com>
2018-05-28 14:48:03 +0100

    completed question 1 and 2 in the app.js file

--- app.js
+++ app.js
@@ -14,3 +14,44 @@
 // HINT: Plan well before you start coding.
 //
 // The Repo name is Sparta-Control-Flow-Story
+
+
+
+// STORY TIME
+
+// Question 1 (alert, variable, prompt, if/elseif, strings)
+alert('You are invited to my house for dinner and are the first to arrive.');
+
+var jollof = prompt("You are presented with a choice of a) Ghana jollof or b) Naija jollof. Which do you pick? (Type 'a' or 'b')");
+
+if (jollof == 'a') {
+    alert('Ghana Jollof, yummy!!! So now your tummy is full and you are happy and satisfied');
+  } else if (jollof == 'b') {
+    alert('Naija jollof???? Ghana jollof is tastier so I served you that anyway...');
+  }
+
+//Question 2 (alert, variable, prompt, switch statements, strings)
+alert('You must be so thirsty now that you are full and satisfied.');
+
+var drink = prompt('If you could drink anything you want, what would it be?').toUpperCase();
+
+switch (drink) {
+  case 'SUPERMALT':
+    alert('The fridge is full, help yourself. The opener is on the fridge. get me one too!')
+    break;
+  case 'WATER':
+    alert('The fridge is full, help yourself.')
+    break;
+  case 'TEA':
+    alert('Go and pop the kettle on. Mugs and teabags are in cabinet above it!')
+    break;
+  default:
+    alert('We nuh \'ave dat! Go look in fridge and see if theres anything you like.')
+}
+
+
+
+// Bridge
+alert("Now let's see how well you know me!");
+
+// Quiz
